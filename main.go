1443c92b29fbc895f14575c9f5e17f7ca7abb5e3
Ram Pandey <8826047538r@gmail.com>
2019-08-18 17:01:44 +0530

    Homepage added

--- main.go
+++ main.go
@@ -13,13 +13,12 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", handlers.Home)
+	r.HandleFunc("/", handlers.Home).Methods("GET")
 	r.HandleFunc("/auth", authenticator.Authenticator).Methods("GET")
 
 	// passing true for secured routes and false for unsecured along with handler function
 	r.HandleFunc("/user/profile", gateway.APIGateway(handlers.Profile, true)).Methods("GET")
 	r.HandleFunc("/microservice/name", gateway.APIGateway(handlers.MicroserviceName, false)).Methods("GET")
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
 
 	// starting server at 8080
 	log.Fatal(http.ListenAndServe(":8080", r))
